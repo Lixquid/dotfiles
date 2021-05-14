631b624ff2638b728d233b9b51ea3638b9b120b4
Lixquid <lixquid@lixquid.com>
2021-05-15 00:23:44 +0100

    Add PowerShell Profile

---
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,12 @@
+$module = Get-Module posh-git -ListAvailable
+if ($module) {
+    Import-Module $module
+}
+
+$module = Get-Module oh-my-posh -ListAvailable
+if ($module) {
+    Import-Module $module
+    Set-Theme Agnoster
+}
+
+. $HOME/.dotfiles/powershell/ProfileFunctions.ps1

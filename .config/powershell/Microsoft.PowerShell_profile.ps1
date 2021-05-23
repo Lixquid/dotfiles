a9a3a80b63825e8dafbf43e191f6500c9f818c48
Lixquid <lixquid@lixquid.com>
2021-05-23 12:58:26 +0100

    Add rust tooling to PATH

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,4 @@
+## Modules #####################################################################
 $private:module = Get-Module posh-git -ListAvailable
 if ($private:module) {
     Import-Module $private:module
@@ -9,4 +10,17 @@ if ($private:module) {
     Set-Theme Agnoster
 }
 
+## Tooling #####################################################################
+
+# Rust
+$private:cargo = $env:CARGO_HOME
+if (-not $private:cargo) {
+    $private:cargo = Join-Path $HOME .cargo
+}
+$private:cargo = Join-Path $private:cargo bin
+if (Test-Path -LiteralPath $private:cargo -PathType Container) {
+    $env:PATH = $private:cargo + ":" + $env:PATH
+}
+
+## Utility #####################################################################
 . $HOME/.dotfiles/powershell/ProfileFunctions.ps1

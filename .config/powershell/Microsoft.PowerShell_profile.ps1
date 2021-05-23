38d0423c8aee798b4109138eb7fde455dc09f695
Lixquid <lixquid@lixquid.com>
2021-05-23 12:59:37 +0100

    Actually fix PowerShell profile variable leaks

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,25 +1,29 @@
-## Modules #####################################################################
-$private:module = Get-Module posh-git -ListAvailable
-if ($private:module) {
-    Import-Module $private:module
-}
+Invoke-Command {
 
-$private:module = Get-Module oh-my-posh -ListAvailable
-if ($private:module) {
-    Import-Module $private:module
-    Set-Theme Agnoster
-}
+    ## Modules #################################################################
+    $module = Get-Module posh-git -ListAvailable
+    if ($module) {
+        Import-Module $module
+    }
 
-## Tooling #####################################################################
+    $module = Get-Module oh-my-posh -ListAvailable
+    if ($module) {
+        Import-Module $module
+        Set-Theme Agnoster
+    }
 
-# Rust
-$private:cargo = $env:CARGO_HOME
-if (-not $private:cargo) {
-    $private:cargo = Join-Path $HOME .cargo
-}
-$private:cargo = Join-Path $private:cargo bin
-if (Test-Path -LiteralPath $private:cargo -PathType Container) {
-    $env:PATH = $private:cargo + ":" + $env:PATH
+    ## Tooling #################################################################
+
+    # Rust
+    $cargo = $env:CARGO_HOME
+    if (-not $cargo) {
+        $cargo = Join-Path $HOME .cargo
+    }
+    $cargo = Join-Path $private:cargo bin
+    if (Test-Path -LiteralPath $cargo -PathType Container) {
+        $env:PATH = $cargo + ":" + $env:PATH
+    }
+
 }
 
 ## Utility #####################################################################

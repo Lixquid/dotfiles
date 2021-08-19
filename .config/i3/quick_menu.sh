6bccfb7a0a408adf2ed747795b48b772d0c193a4
Lixquid <lixquid@lixquid.com>
2021-08-19 17:47:19 +0100

    Add open non-vpn window to quick menu

--- .config/i3/quick_menu.sh
+++ .config/i3/quick_menu.sh
@@ -2,11 +2,21 @@
 
 OPT=$(dmenu -l 5 -p "Quick Menu" <<EOF
 Restart Mullvad Daemon
+Open Non-Mullvad Window
 EOF
 )
 
 case "$OPT" in
     "Restart Mullvad Daemon")
         xfce4-terminal --command='systemctl restart mullvad-daemon'
+        ;;
+    "Open Non-Mullvad Window")
+        mullvad disconnect
+        mullvad always-require-vpn set off
+        rm -rf /tmp/tempfirefoxprofile
+        mkdir -p /tmp/tempfirefoxprofile
+        firefox --private-window --profile /tmp/tempfirefoxprofile http://neverssl.com
+        mullvad connect
+        mullvad always-require-vpn set on
         ;;
 esac
